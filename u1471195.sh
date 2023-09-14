53ba86af3e0a68bd927fc765a8bb032e3b8e9be6
Dhruv Meduri <dhruvm@dhruvs-air.uconnect.utah.edu>
2023-09-14 13:34:20 -0600

    Second

--- u1471195.sh
+++ u1471195.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=40GB
 #SBATCH --mail-user=u1471195@umail.utah.edu
 #SBATCH --mail-type=FAIL,END
-#SBATCH -o assignment_1-%j
+#SBATCH -o assignment_2-%j
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate CS6966
@@ -20,6 +20,6 @@ export TRANSFORMERS_CACHE="/scratch/general/vast/u1471195/huggingface_cache"
 export HF_DATASETS_CACHE="/scratch/general/vast/u1471195/huggingface_cache"
 
 
-OUT_DIR=/scratch/general/vast/u1471195/cs6966/assignment1/models
+OUT_DIR=/scratch/general/vast/u1471195/cs6966/assignment2/models
 mkdir -p ${OUT_DIR}
-python ~/6966/assignment2_incomplete.py --output_dir ${OUT_DIR} --seed 1471195
+python ~/CS6966_A2/assignment2_incomplete.py --output_dir ${OUT_DIR} --seed 1471195
